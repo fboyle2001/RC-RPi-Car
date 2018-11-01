ed1989a3b283c915385b7d94204f5d92f37589f7
fboyle2001 <fboyle2001@users.noreply.github.com>
2018-11-01 18:00:50 +0000

    Moved assisted U turn to seperate thread to allow override

--- rpi/java/src/com/finlay/rc/components/AssistedTurn.java
+++ rpi/java/src/com/finlay/rc/components/AssistedTurn.java
@@ -17,36 +17,38 @@ public class AssistedTurn {
 	}
 	
 	public static void performUTurn(int speed, int duration) {
-		PiconZero.getInstance().forward(speed);
-		int left = speed;   //-speed
-		
-		while(left != -speed) {
-			if(override) {
-				return;
-			}
+		new Thread(() -> {
+			PiconZero.getInstance().forward(speed);
+			int left = speed;   //-speed
 			
-			PiconZero.getInstance().setMotorSpeed(0, left);
-			left -= 10;
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+			while(left != -speed) {
+				if(override) {
+					return;
+				}
+				
+				PiconZero.getInstance().setMotorSpeed(0, left);
+				left -= 10;
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
-		}
-
-		while(left != speed) {
-			if(override) {
-				return;
+	
+			while(left != speed) {
+				if(override) {
+					return;
+				}
+				
+				PiconZero.getInstance().setMotorSpeed(0, left);
+				left += 10;
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
-			
-			PiconZero.getInstance().setMotorSpeed(0, left);
-			left += 10;
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+		}, "AssistedUTurn").start();
 	}
 	
 	public static void turnRight(int speed, int duration) {
