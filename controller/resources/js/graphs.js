ddcefa08f87010f89b45d8e988d836fb6e3ab1c1
fboyle2001 <finlayboyle2001@gmail.com>
2019-05-26 19:57:50 +0100

    Graph the components of the readings

--- controller/resources/js/graphs.js
+++ controller/resources/js/graphs.js
@@ -3,31 +3,257 @@ $(function () {
 });
 
 var maximumLength = 40;
-var dataPoints = {
-  aX: [],
-  aY: [],
-  aZ: [],
-  gX: [],
-  gY: [],
-  gZ: []
-};
+var updateQueue = 0;
+var charts = {};
 
 function processDataPoints(readings) {
   Object.keys(readings).forEach(function (key) {
-    if(dataPoints[key].length == maximumLength) {
-      dataPoints[key].shift();
-    }
-
-    dataPoints[key].push(readings[key]);
+    var newPos = charts[key].data.datasets[0].data.length;
+    charts[key].data.datasets[0].data[newPos] = readings[key];
+    charts[key].data.labels[newPos] = newPos;
+    charts[key].update();
   });
-
-  updateGraphs();
-}
-
-function updateGraphs() {
-  
 }
 
 function setupGraphs() {
-
+  charts["aX"] = new Chart($("#graph_acc_x"), {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [{
+        label: "Sensor Data",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 5,
+        pointHitRadius: 10,
+        data: []
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      title: {
+        display: true,
+        text: "Accelerometer X"
+      }
+    }
+  });
+  charts["aY"] = new Chart($("#graph_acc_y"), {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [{
+        label: "Sensor Data",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 5,
+        pointHitRadius: 10,
+        data: []
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      title: {
+        display: true,
+        text: "Accelerometer Y"
+      }
+    }
+  });
+  charts["aZ"] = new Chart($("#graph_acc_z"), {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [{
+        label: "Sensor Data",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 5,
+        pointHitRadius: 10,
+        data: []
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      title: {
+        display: true,
+        text: "Accelerometer Z"
+      }
+    }
+  });
+  charts["gX"] = new Chart($("#graph_gyro_x"), {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [{
+        label: "Sensor Data",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 5,
+        pointHitRadius: 10,
+        data: []
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      title: {
+        display: true,
+        text: "Gyroscope X"
+      }
+    }
+  });
+  charts["gY"] = new Chart($("#graph_gyro_y"), {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [{
+        label: "Sensor Data",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 5,
+        pointHitRadius: 10,
+        data: []
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      title: {
+        display: true,
+        text: "Gyroscope Y"
+      }
+    }
+  });
+  charts["gZ"] = new Chart($("#graph_gyro_z"), {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [{
+        label: "Sensor Data",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 5,
+        pointHitRadius: 10,
+        data: []
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      title: {
+        display: true,
+        text: "Gyroscope Z"
+      }
+    }
+  });
 }
